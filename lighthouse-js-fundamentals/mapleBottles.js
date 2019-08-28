bf5cf2b66e9b31a223ae6bff6fefffec62625c85
Fred <thecanuckatheist@gmail.com>
2019-08-28 03:27:21 +0000

    Bottle crate exercise

---
+++ lighthouse-js-fundamentals/mapleBottles.js
@@ -0,0 +1,10 @@
+function howManyHundreds(bottles) {
+    var pack = Math.floor(bottles / 100);
+    return pack;
+}
+
+console.log(howManyHundreds(1000));
+console.log(howManyHundreds(894));
+console.log(howManyHundreds(520));
+console.log(howManyHundreds(99));
+console.log(howManyHundreds(0));
